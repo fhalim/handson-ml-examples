60dc4719efe69ea898993d8f5ac2ec60953cbb19
Fawad Halim <fawad@fawad.net>
2017-08-26 20:44:35 -0500

    Flush

--- chapter02/housing.py
+++ chapter02/housing.py
@@ -1,6 +1,10 @@
+"""Chapter 2 examples from book
+"""
+#%%
 import os
 import tarfile
 from six.moves import urllib
+import pandas as pd
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = "datasets/housing"
@@ -11,4 +15,103 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
         os.makedirs(housing_path)
     tgz_path=os.path.join(housing_path, "housing.tgz")
     urllib.request.urlretrieve(housing_url, tgz_path)
-    housing_tgz = tarfile
+    with tarfile.open(tgz_path) as housing_tgz:
+        housing_tgz.extractall(path=housing_path)
+
+fetch_housing_data()
+
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path=os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+housing = load_housing_data()
+housing.head()
+
+#%%
+housing.info()
+
+#%%
+# Analyze data
+housing["ocean_proximity"].value_counts()
+#%%
+housing.describe()
+
+#%%
+# Feature correlation
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+housing.hist(bins=50, figsize=(20,15))
+plt.show()
+
+#%%
+# Test/Train split
+import numpy as np
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+
+train_set, test_set = split_train_test(housing, 0.2)
+
+print(len(train_set), "train + ", len(test_set), "test")
+
+#%%
+# Stable training set
+import hashlib
+
+
+def test_set_check(identifier, test_ration, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+    
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[-in_test_set], data.loc[in_test_set]
+
+
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+housing["income_cat"].value_counts() / len(housing)
+
+housing["income_cat"].describe()
+
+#%%
+# Looking for correlation
+corr_matrix = housing.corr()
+print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+from pandas.tools.plotting import scatter_matrix
+import matplotlib
+matplotlib.style.use('ggplot')
+scatter_matrix(housing[["median_house_value", "median_income", "total_rooms", "housing_median_age"]], figsize=(12, 8))
+
+#%%
+# Zoom into median income correlation
+housing.plot(kind = 'scatter', x = 'median_income', y='median_house_value', alpha=0.1)
+
+#%%
+# Derived attributed
+
+housing['rooms_per_household'] = housing['total_rooms'] / housing['households']
+housing['bedrooms_per_rooms'] = housing['total_bedrooms'] / housing['total_rooms']
